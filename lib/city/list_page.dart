31a04613e9ab448851fcb85a6273808a53e88a06
takec18 <takec18@gmail.com>
2023-11-30 15:44:47 +0900

    JSONから市区町村データを取得

--- lib/city/list_page.dart
+++ lib/city/list_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../env.dart';
 import 'detail_page.dart';
 
 class CityListPage extends StatefulWidget {
@@ -10,58 +14,56 @@ class CityListPage extends StatefulWidget {
 }
 
 class _CityListPageState extends State<CityListPage> {
-  late Future<void> _future;
+  late Future<String> _citiesFuture;
 
   @override
   void initState() {
     super.initState();
-    _future = Future.delayed(const Duration(seconds: 3));
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = '/api/v1/cities';
+    final headers = {
+      'X-API-KEY': Env.resasApiKey,
+    };
+    _citiesFuture = http
+        .get(
+          Uri.https(host, endpoint),
+          headers: headers,
+        )
+        .then((res) => res.body);
   }
 
   @override
   Widget build(BuildContext context) {
-    final cities = [
-      '札幌市',
-      '横浜市',
-      '川崎市',
-      '名古屋市',
-      '京都市',
-      '大阪市',
-      '堺市',
-      '神戸市',
-      '岡山市',
-      '広島市',
-      '北九州市',
-      '福岡市',
-      '熊本市',
-      '那覇市',
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: FutureBuilder<void>(
-        future: _future,
+      body: FutureBuilder<String>(
+        future: _citiesFuture,
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              return ListView(
-                children: [
-                  for (final city in cities)
-                    ListTile(
-                      title: Text(city),
-                      subtitle: const Text('政令指定都市'),
-                      trailing: const Icon(Icons.navigate_next),
-                      onTap: () {
-                        Navigator.of(context).push<void>(
-                          MaterialPageRoute(
-                            builder: (context) => CityDetailPage(city: city),
+              final json = jsonDecode(snapshot.data!)['result'] as List;
+              final items = json.cast<Map<String, dynamic>>();
+              return ListView.builder(
+                itemCount: items.length,
+                itemBuilder: (context, index) {
+                  final item = items[index];
+                  return ListTile(
+                    title: Text(item['cityName'] as String),
+                    subtitle: const Text('政令指定都市'),
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      Navigator.of(context).push<void>(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(
+                            city: item['cityName'] as String,
                           ),
-                        );
-                      },
-                    ),
-                ],
+                        ),
+                      );
+                    },
+                  );
+                },
               );
             case ConnectionState.none:
             case ConnectionState.waiting:
