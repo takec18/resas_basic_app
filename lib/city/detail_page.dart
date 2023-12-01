98880569959d52317dae75407ce2f3e3f84d08fa
takec18 <takec18@gmail.com>
2023-12-01 15:33:07 +0900

    詳細画面にタイトルウィジェットを追加

--- lib/city/detail_page.dart
+++ lib/city/detail_page.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:resas_basic_app/city/annual_municipality_tax.dart';
+import 'package:resas_basic_app/city/city.dart';
 
 import '../env.dart';
 
 class CityDetailPage extends StatefulWidget {
   const CityDetailPage({super.key, required this.city});
 
-  final String city;
+  final City city;
 
   @override
   State<CityDetailPage> createState() => _CityDetailPageState();
@@ -29,8 +30,8 @@ class _CityDetailPageState extends State<CityDetailPage> {
     };
 
     final param = {
-      'prefCode': '14',
-      'cityCode': '14131',
+      'prefCode': widget.city.prefCode.toString(),
+      'cityCode': widget.city.cityCode,
     };
 
     _municipalityTaxesFuture = http
@@ -46,42 +47,59 @@ class _CityDetailPageState extends State<CityDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(widget.city),
+          title: Text(widget.city.cityName),
         ),
-        body: FutureBuilder<String>(
-          future: _municipalityTaxesFuture,
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.done:
-                final result = jsonDecode(snapshot.data!)['result']
-                    as Map<String, dynamic>;
-                final data = result['data'] as List;
-                final items = data.cast<Map<String, dynamic>>();
-                final taxes =
-                    items.map(AnnualMunicipalityTax.fromJson).toList();
-                return ListView.separated(
-                  itemCount: taxes.length,
-                  separatorBuilder: (context, index) => const Divider(),
-                  itemBuilder: (context, index) {
-                    final tax = taxes[index];
-                    return ListTile(
-                      title: Text('${tax.year}年'),
-                      trailing: Text(
-                        _formatTaxLabel(tax.value),
-                        style: Theme.of(context).textTheme.bodyLarge,
-                      ),
+        body: Column(children: [
+          Container(
+            width: double.infinity,
+            color: Theme.of(context).colorScheme.primaryContainer,
+            child: const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              child: Text(
+                '一人当たり地方税',
+              ),
+            ),
+          ),
+          Expanded(
+            child: FutureBuilder<String>(
+              future: _municipalityTaxesFuture,
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.done:
+                    final result = jsonDecode(snapshot.data!)['result']
+                        as Map<String, dynamic>;
+                    final data = result['data'] as List;
+                    final items = data.cast<Map<String, dynamic>>();
+                    final taxes = items
+                        .map(AnnualMunicipalityTax.fromJson)
+                        .toList()
+                        .reversed
+                        .toList();
+                    return ListView.separated(
+                      itemCount: taxes.length,
+                      separatorBuilder: (context, index) => const Divider(),
+                      itemBuilder: (context, index) {
+                        final tax = taxes[index];
+                        return ListTile(
+                          title: Text('${tax.year}年'),
+                          trailing: Text(
+                            _formatTaxLabel(tax.value),
+                            style: Theme.of(context).textTheme.bodyLarge,
+                          ),
+                        );
+                      },
                     );
-                  },
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                  case ConnectionState.active:
+                }
+                return const Center(
+                  child: CircularProgressIndicator(),
                 );
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          },
-        ));
+              },
+            ),
+          ),
+        ]));
   }
 
   String _formatTaxLabel(int value) {
